eb43248e772bdbcd2f5f30764ccb6c07caaa4521
pallavikyama <kyamap98@gmail.com>
2020-10-12 18:49:40 +0530

    Added:New welcome message is added

---
+++ MyBinaryNode.java
@@ -0,0 +1,7 @@
+package com.blz.binarytree;
+
+public class MyBinaryNode {
+	public static void main(String[] args) {
+		System.out.println("Welcome to creating Binary_Search_Tree data using Java Generics");
+	}
+}
