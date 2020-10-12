a0a00c20d734fcfcf04e73d2b46b781e4accd99b
pallavikyama <kyamap98@gmail.com>
2020-10-12 20:33:51 +0530

    Added:search method is used to find a node in the BST. Also related testcases are validated.

--- BinaryTreeTest.java
+++ BinaryTreeTest.java
@@ -1,11 +1,9 @@
 package com.blz.binarytree;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class BinaryTreeTest {
-
 	@Test
 	public void binaryTreeTestUC01() {
 		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
@@ -18,5 +16,27 @@ public class BinaryTreeTest {
 		assertEquals(leftValue, 30);
 		int rightValue = myBinaryTree.getRoot().right.key;
 		assertEquals(rightValue, 70);
+	}
+
+	@Test
+	public void binaryTreeTestUC02() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int rootValue = myBinaryTree.getRoot().key;
+		assertEquals(rootValue, 56);
+		assertEquals(3, myBinaryTree.getSize());
+	}
+
+	@Test
+	public void binaryTreeTestUC03() {
+		MyBinaryTree<Integer> myBinaryTree = new MyBinaryTree<Integer>();
+		myBinaryTree.add(56);
+		myBinaryTree.add(30);
+		myBinaryTree.add(70);
+		int rootValue = myBinaryTree.getRoot().key;
+		assertEquals(rootValue, 56);
+		assertEquals(null, myBinaryTree.search(myBinaryTree.getRoot(), 63));
 	}
 }
--- MyBinaryTree.java
+++ MyBinaryTree.java
@@ -22,4 +22,26 @@ public class MyBinaryTree<E extends Comparable<E>> {
 			current.right = addRecursively(current.right, key);
 		return current;
 	}
+
+	// GET SIZE OF THIS BINARY TREE
+	public int getSize() {
+		return this.getSizeRecursively(this.root);
+	}
+
+	private int getSizeRecursively(MyBinaryNode<E> current) {
+		if (current == null)
+			return 0;
+		else
+			return 1 + this.getSizeRecursively(current.left) + this.getSizeRecursively(current.right);
+	}
+
+	// SEARCH FOR THE SPECIFIED NODE IN THE BST USING ITS KEY VALUE
+	public MyBinaryNode<E> search(MyBinaryNode<E> current, E key) {
+		if (current == null || current.key == key)
+			return current;
+		int compareResult = key.compareTo(current.key);
+		if (compareResult < 0)
+			return current.left = search(current.left, key);
+		return search(current.right, key);
+	}
 }
